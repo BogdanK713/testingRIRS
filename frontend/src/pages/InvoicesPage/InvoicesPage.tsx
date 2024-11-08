2b8bc32a4dfcfd7d7f1494b011de166e8d34865e
Bogdan Kascelan <bogdan.kascelan@student.um.si>
2024-11-08 16:11:31 +0100

    Added feature for creating and updating invoices UI

--- frontend/src/pages/InvoicesPage/InvoicesPage.tsx
+++ frontend/src/pages/InvoicesPage/InvoicesPage.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import AddUpdateInvoice, { InvoiceData } from "./AddUpdateInvoice";
 
 const InvoicesPage: React.FC = () => {
-	return <div></div>;
+    const handleSaveInvoice = (invoiceData: InvoiceData) => {
+        console.log("Invoice saved:", invoiceData);
+    };
+
+    // Dummy data for testing the update mode
+    const dummyInvoice: InvoiceData = {
+        name: "Dummy Invoice",
+        amount: 100,
+        date: "2024-01-01",
+        dueDate: "2024-02-01",
+        payer: "Sample Payer",
+        statusSent: true,
+        statusPaid: false,
+    };
+
+    return (
+        <div>
+            <h2>Manage Invoices</h2>
+            {/* To test "add" mode */}
+            {/* <AddUpdateInvoice onSave={handleSaveInvoice} mode="add" /> */}
+            
+            {/* To test "edit" mode with initial data */}
+            <AddUpdateInvoice onSave={handleSaveInvoice} mode="edit" initialData={dummyInvoice} />
+        </div>
+    );
 };
 
 export default InvoicesPage;
